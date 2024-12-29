002e31e868ef3ee2e653a069f4655b6e4092da66
Ashwin Mahesh <ashwin.mahesh@instabase.com>
2024-12-28 19:16:11 -0500

    Use env var API base

--- frontend/dnd-ai/src/api/http.ts
+++ frontend/dnd-ai/src/api/http.ts
@@ -5,8 +5,8 @@ import { SelectedCampaignKey } from '@/constants/AsyncStorageKeys';
 import AsyncStorage, { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
 const http = axios.create({
-  // baseURL: `${Config.API_BASE}/api/v1`,
-  baseURL: `http://localhost:6875/api/v1`,
+  baseURL: `${Config.API_BASE}/api/v1`,
+  // baseURL: `http://localhost:6875/api/v1`,
   timeout: 25000,
 });
 
