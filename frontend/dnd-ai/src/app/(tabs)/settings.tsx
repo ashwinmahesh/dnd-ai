d00f09077c8c80e3e149bbbc4526b382830f5cc5
Ashwin Mahesh <ashwin.mahesh@instabase.com>
2025-01-27 22:05:03 -0500

    Delete account working

--- frontend/dnd-ai/src/app/(tabs)/settings.tsx
+++ frontend/dnd-ai/src/app/(tabs)/settings.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Layout, Text, useTheme } from '@ui-kitten/components';
 import { firebaseAuth } from '@/FirebaseConfig';
+import auth from '@react-native-firebase/auth';
 import { Alert } from 'react-native';
 import { useRouter } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -14,6 +15,9 @@ export default function Page() {
   const theme = useTheme();
 
   const [cacheClearLoading, setCacheClearLoading] = useState(false);
+  const [deleteAccountLoading, setDeleteAccountLoading] = useState(false);
+
+  const [deleteAccountErr, setDeleteAccountErr] = useState('');
 
   const handleLogout = async () => {
     const confirm = await new Promise((resolve) => {
@@ -50,6 +54,74 @@ export default function Page() {
     }
   };
 
+  const handlePasswordSubmit = async (password: string) => {
+    const user = firebaseAuth.currentUser;
+    const credential = auth.EmailAuthProvider.credential(user.email, password);
+    try {
+      await user.reauthenticateWithCredential(credential);
+      return true;
+    } catch (err) {
+      console.warn('Invalid password');
+      setDeleteAccountErr('Account not deleted - invalid password entered');
+      return false;
+    }
+  };
+
+  const handleDelete = async () => {
+    setDeleteAccountErr('');
+    const confirm = await new Promise((resolve) => {
+      Alert.alert('Delete Account', 'Are you sure you want to delete your account? This action cannot be undone', [
+        {
+          text: 'Confirm',
+          onPress: () => {
+            Alert.prompt(
+              'Confirm Deletion',
+              'Please enter your password to delete your account:',
+              [
+                {
+                  text: 'Cancel',
+                  style: 'cancel',
+                },
+                {
+                  text: 'Delete',
+                  onPress: async (password: string) => {
+                    const ok = await handlePasswordSubmit(password);
+                    resolve(ok);
+                  },
+                  style: 'destructive',
+                },
+              ],
+              'secure-text'
+            );
+          },
+        },
+        { text: 'Cancel', onPress: () => resolve(false), style: 'cancel' },
+      ]);
+    });
+    if (!confirm) return;
+    setDeleteAccountLoading(true);
+    const user = firebaseAuth.currentUser;
+    if (!user) {
+      console.error('no user found');
+      setDeleteAccountLoading(false);
+      return;
+    }
+
+    try {
+      await user.delete();
+      try {
+        await AsyncStorage.multiRemove(AllKeys);
+      } catch (err) {
+        console.error('failed to clear cache:', err.ToString());
+      }
+      setDeleteAccountLoading(false);
+      router.replace('/loading');
+    } catch (err) {
+      Alert.alert('Failed to Delete Account', err.ToString());
+      setDeleteAccountLoading(false);
+    }
+  };
+
   return (
     <Layout
       className="flex flex-1 align-middle"
@@ -64,6 +136,7 @@ export default function Page() {
         >
           More Features Coming Soon...
         </Text>
+        {deleteAccountErr && <Text status="danger">{deleteAccountErr}</Text>}
       </Layout>
       <Logo
         image="protector"
@@ -91,6 +164,14 @@ export default function Page() {
           disabled={cacheClearLoading}
         >
           Clear Cache
+        </Button>
+        <Button
+          appearance="ghost"
+          status="danger"
+          onPress={handleDelete}
+          disabled={deleteAccountLoading}
+        >
+          Delete Account
         </Button>
       </Layout>
     </Layout>
